623b99c282fbe3b82af4cdcc96baf6fa11c9f477
Olga Lyashevska <olga.lyashevska@gmail.com>
2023-09-28 21:18:09 +0200

    Create conversion.py

---
+++ conversion.py
@@ -0,0 +1,50 @@
+def convert_temperature(temperature, unit):
+    if unit == "F":
+        # Convert Fahrenheit to Celsius
+        celsius = (temperature - 32) * (5 / 9)
+        if celsius < -273.15:
+            # Invalid temperature, below absolute zero
+            return "Invalid temperature"
+        else:
+            # Convert Celsius to Kelvin
+            kelvin = celsius + 273.15
+            if kelvin < 0:
+                # Invalid temperature, below absolute zero
+                return "Invalid temperature"
+            else:
+                fahrenheit = (celsius * (9 / 5)) + 32
+                if fahrenheit < -459.67:
+                    # Invalid temperature, below absolute zero
+                    return "Invalid temperature"
+                else:
+                    return celsius, kelvin
+    elif unit == "C":
+        # Convert Celsius to Fahrenheit
+        fahrenheit = (temperature * (9 / 5)) + 32
+        if fahrenheit < -459.67:
+            # Invalid temperature, below absolute zero
+            return "Invalid temperature"
+        else:
+            # Convert Celsius to Kelvin
+            kelvin = temperature + 273.15
+            if kelvin < 0:
+                # Invalid temperature, below absolute zero
+                return "Invalid temperature"
+            else:
+                return fahrenheit, kelvin
+    elif unit == "K":
+        # Convert Kelvin to Celsius
+        celsius = temperature - 273.15
+        if celsius < -273.15:
+            # Invalid temperature, below absolute zero
+            return "Invalid temperature"
+        else:
+            # Convert Celsius to Fahrenheit
+            fahrenheit = (celsius * (9 / 5)) + 32
+            if fahrenheit < -459.67:
+                # Invalid temperature, below absolute zero
+                return "Invalid temperature"
+            else:
+                return celsius, fahrenheit
+    else:
+        return "Invalid unit"
